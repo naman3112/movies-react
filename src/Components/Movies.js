db59c68cc0dcfe18b24c9b554870f9783bc3ebef
Naman Arora <namanarora311@gmail.com>
2022-02-20 11:13:10 +0530

    added search functionality

--- src/Components/Movies.js
+++ src/Components/Movies.js
@@ -4,21 +4,42 @@ export default class Movies extends Component {
     constructor() {
         super();
         this.state = {
-            movies: getMovies()
+            movies: getMovies(),
+            currSearchText: ''
         }
     }
-  deleteHandler=(id)=>{
-     
-   let newMovies=  this.state.movies.filter((movie)=>{
-      
-            return movie._id!=id
-     })
-  this.setState({movies: newMovies});
-  
+    handleChange=(e)=>{
+        let val=e.target.value;
+        console.log(val);
+    this.setState({
+        currSearchText: val
+    })
+    }
+    deleteHandler = (id) => {
+
+        let newMovies = this.state.movies.filter((movie) => {
+            return movie._id != id
+        })
+        this.setState({ movies: newMovies });
+
     }
 
 
     render() {
+        console.log('render')
+        let {movies,currSearchText}=this.state;
+        let filteredArr=[];
+        if(currSearchText==''){
+            filteredArr=movies;
+        }else{
+            filteredArr=movies.filter(function(movieObj){
+                let title=movieObj.title.toLowerCase();
+                console.log(title);
+                return title.includes(currSearchText.toLowerCase());
+            })
+        }
+
+     
         return (
             <div className='container'>
                 <div className='row'>
@@ -26,6 +47,7 @@ export default class Movies extends Component {
                         hello-col-3
                     </div>
                     <div className='col-9'>
+                        <input type='search' value={this.state.currSearchText} onChange={this.handleChange}></input>
                         <table className="table">
                             <thead>
                                 <tr>
@@ -39,15 +61,15 @@ export default class Movies extends Component {
                             </thead>
                             <tbody>
                                 {
-                                    this.state.movies.map((movieObj) => {
+                                    filteredArr.map((movieObj) => {
                                         return (
-                                            <tr scope='row'>
+                                            <tr scope='row' key={movieObj._id}>
                                                 <td></td>
                                                 <td> {movieObj.title} </td>
                                                 <td> {movieObj.genre.name} </td>
                                                 <td>{movieObj.numberInStock}</td>
                                                 <td>{movieObj.dailyRentalRate}</td>
-                                                <td><button onClick={()=>{return this.deleteHandler(movieObj._id)}} type="button" className="btn btn-danger">Delete</button>
+                                                <td><button onClick={() => { return this.deleteHandler(movieObj._id) }} type="button" className="btn btn-danger">Delete</button>
                                                 </td>
                                             </tr>
                                         )
